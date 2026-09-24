4cc470a5daef9b8bffc034dd49ec28ed8930e0fe
Alexander Rafael Martínez Morillo <alkut202@gmail.com>
2026-09-24 11:10:51 -0500

    chore(build): agregar web.config para IIS y actualizar configuracion de build y gitignore

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { errorApiInterceptor } from './core/interceptors/error-api.interceptor';
 import { ESTUDIANTE_REPOSITORY } from './domain/repositories/estudiante.repository';
@@ -11,7 +12,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding()),
+    { provide: APP_BASE_HREF, useValue: '/appTitulacion/' },
+    provideRouter(routes, withComponentInputBinding(), withHashLocation()),
     provideHttpClient(withInterceptors([jwtInterceptor, errorApiInterceptor])),
     provideAnimationsAsync(),
 
